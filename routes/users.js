6ec6171bcd04c40fa1d17d5ab2551724ddf51183
pipinho <eltjon.sulaj@gmail.com>
2019-01-22 21:33:20 +0100

    Add endpoint for users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const _ = require('lodash');
+const {User, validate} = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res) => {
+    const users = await User.find().sort('name');
+    res.send(users);
+});
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('User already registered.')
+
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+
+    await user.save();
+
+    res.send(_.pick(user, ['_id', 'name', 'email']));
+});
+
+router.get('/:id', async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if (!user) return res.status(404).send('The User with the given ID was not found.');
+
+    res.send(user);
 });
 
 module.exports = router;
