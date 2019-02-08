f155e8b3cd0cc72041b36f6296fbbcaba9d53563
pipinho <eltjon.sulaj@gmail.com>
2019-02-08 23:01:26 +0100

    Add authorization middlware

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const auth = require('../middleware/auth');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const {User, validate} = require('../models/user');
@@ -10,7 +11,7 @@ router.get('/', async (req, res) => {
     res.send(users);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
