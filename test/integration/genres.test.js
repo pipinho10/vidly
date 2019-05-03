5cf52ba00415713adb52e94f14d389e8f7e39bee
pipinho <eltjon.sulaj@gmail.com>
2019-05-03 23:22:15 +0200

    Add integration tests for getting genres by id api

--- test/integration/genres.test.js
+++ test/integration/genres.test.js
@@ -5,7 +5,10 @@ let server;
 
 describe('/api/genres', () => {
     beforeEach(() => {server = require('../../index');});
-    afterEach(() => {server.close();});
+    afterEach(async () => {
+        server.close();
+        await Genre.remove({});
+    });
 
    describe('GET /', () => {
        it('should return all genres', async () => {
@@ -18,6 +21,26 @@ describe('/api/genres', () => {
 
            expect(res.status).toBe(200);
            expect(res.body.length).toBe(2);
+           expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
+           expect(res.body.some(g => g.name === 'genre2')).toBeTruthy();
+       });
+   });
+
+   describe('GET /:id', () => {
+      it('should return a genre if valid id is passed', async () => {
+          const genre = new Genre({name: 'genre1'});
+          await genre.save();
+
+          const res = await request(server).get('/api/genres/' + genre._id);
+
+          expect(res.status).toBe(200);
+          expect(res.body).toHaveProperty('name', genre.name);
+      });
+
+       it('should return 404 if invalid id is passed', async () => {
+           const res = await request(server).get('/api/genres/1');
+
+           expect(res.status).toBe(404);
        });
    });
 });
