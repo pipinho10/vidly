209fc89cc1e343ccb53990bc95fa9339c5b79645
pipinho <eltjon.sulaj@gmail.com>
2019-05-03 21:49:33 +0200

    Add integration test should return all genres

--- test/integration/genres.test.js
+++ test/integration/genres.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const {Genre} = require('../../models/genre');
 
 let server;
 
@@ -8,9 +9,15 @@ describe('/api/genres', () => {
 
    describe('GET /', () => {
        it('should return all genres', async () => {
+           await Genre.collection.insertMany([
+               {name: 'genre1'},
+               {name: 'genre2'}
+           ]);
+
            const res = await request(server).get('/api/genres');
 
            expect(res.status).toBe(200);
+           expect(res.body.length).toBe(2);
        });
    });
 });
